6347ee8bab28bed20182250d2ec533c245cec474
ytcraftnew <ytcraft.learn@gmail.com>
2026-07-28 10:20:40 +0700

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ytcraft-indonesia',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      max_memory_restart: '1G',
+    },
+  ],
+}
